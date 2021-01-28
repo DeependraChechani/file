afef2370628ddc0e8ba9f52432810288ca2e06c3
DeependraChechani <77540018+DeependraChechani@users.noreply.github.com>
2021-01-28 17:28:41 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,8 @@
+
+list1=list(map(int, input("Enter numbers separated by space: ").split()))
+for num in list1: 
+      
+    # checking condition 
+    if num >= 0: 
+       print(num, end = " ") 
+    
